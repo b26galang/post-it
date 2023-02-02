b25a5a622916a89213bd47e1e09a1a6e4c572b6a
Bridget Galang <galanbri@gmail.com>
2023-02-02 14:47:27 -0800

    post-it

--- src/specs/post-it-reducer.specs.ts
+++ src/specs/post-it-reducer.specs.ts
@@ -1,1 +1,12 @@
-import {PostItState, PostItRedcucer}
+import { PostItState, postItReducer } from "../post-it-reducer"
+
+test("SET_NOTE", ()=>{
+    const PostItState = {
+        currentNote: "",
+        allNotes: []
+    }
+
+    const nextState = postItReducer(PostItState, {type: "SET_NOTE", payload: "Hello"});
+    expect(nextState.currentNote)
+})
+
